d6201328d06c7c30d26e63bf04616e327f02fb6d
sabts <sabrinataraschi@gmail.com>
2025-06-04 19:59:02 +0200

    .

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const cors = require('cors');
+const cors = require("cors");
+const usersRoutes = require("./routes/users.routes");
 
 const corsOptions = {
-  origin: '*', // Orígenes permitidos (cuando esté en un dominio real, lo cambiaremos por ese dominio)
-  methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'] // Headers permitidos
+  origin: "*", // Orígenes permitidos (cuando esté en un dominio real, lo cambiaremos por ese dominio)
+  methods: ["GET", "POST", "PATCH", "DELETE"], // Métodos permitidos
+  allowedHeaders: ["Content-Type", "Authorization"], // Headers permitidos
 };
 
 app.use(cors(corsOptions));
+app.use("/api/users", usersRoutes);
 app.use(express.json());
 
 app.listen(port, () => {
--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -2,4 +2,9 @@ const express = require("express");
 const usersRoutes = express.Router();
 const usersController = require("../controllers/users.controller");
 
-usersRoutes.get("/", usersController);
+usersRoutes.get("/", usersController.getAllData);
+usersRoutes.get("/:id", usersController.getDataById);
+usersRoutes.patch("/:id", usersController.updateDataById);
+usersRoutes.delete("/:id", usersController.deleteDataById);
+
+module.exports = usersRoutes;
