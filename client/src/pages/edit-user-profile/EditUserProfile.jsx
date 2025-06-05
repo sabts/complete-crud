be2c0f3143d6f170d15de2c2b8c96e2378f0c192
sabts <sabrinataraschi@gmail.com>
2025-06-05 02:01:18 +0200

    Create EditUserProfile.jsx

---
+++ client/src/pages/edit-user-profile/EditUserProfile.jsx
@@ -0,0 +1,11 @@
+const EditUserProfile = () => {
+  return  <form>
+    <label>Active</label>
+    <input type="checkbox"/>
+    <input type="text">name</input>
+    <span>email</span>
+    <span>date</span>
+    <span>phone</span>
+    <input type="submit" value="save user" />
+    </form>
+}
